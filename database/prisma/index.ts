b8fffa853dd0f9843993acb4546b7ebbb1041bc4
Juliana <aragaopintojuli@gmail.com>
2022-04-14 22:42:32 -0300

    fix

--- database/prisma/index.ts
+++ database/prisma/index.ts
@@ -4,8 +4,7 @@ const prisma = new PrismaClient();
 // Query com o orm puro
 // prisma.Pessoa.findMany().then(console.log);
 
-// Query usando Raw Qqueries
-
+// Query usando Raw Queries
 async function DB() {
   // Inserção de um dado
   // await prisma.$queryRaw`
